77241e633e16ab74fcb631ab36813abf9c9cf858
Dan Lavoie <dan.r.lavoie@gmail.com>
2014-10-23 00:22:57 -0400

    Added Regex tokenizer to remove_punct_regex function. Removes any punctuation from a text.

--- src/ops/remove_punct.py
+++ src/ops/remove_punct.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 """
 Removes all nonessential punctuation from a text.
-(Leaves elements such as the apostrophe in O'Rourke)
+Returns the text as a single string separated by spaces.
+"""
+from nltk.tokenize import RegexpTokenizer
+class remove_punct:
+    def run(self, data):
+        tokenizer = RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps=True)
+        return " ".join(tokenizer.tokenize(data))
+
+"""
+Test code follows below: to be added to unit test suite.     
+
+s = '''He said,"that's it." *u* Hello, World. O'Rourke is rockin'.'''
+a = remove_punct()
+print(a.run(s))
 """
