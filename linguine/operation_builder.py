e0e4380bd5d63725e2a54ad00b11b830037e8449
petermikitsh <peter.mikitsh@gmail.com>
2014-11-09 23:06:07 -0500

    fix bug in operation builder

--- linguine/operation_builder.py
+++ linguine/operation_builder.py
@@ -1,10 +1,10 @@
 import sys
 from linguine.transaction_exception import TransactionException
+from linguine.ops.tfidf import Tfidf
 from linguine.ops.no_op import NoOp
 
 def get_operation_handler(operation):
-    if operation in globals():
-        constructor = globals()[operation]
-        return constructor()
+    if operation == 'tfidf':
+        return Tfidf()
     else:
         raise TransactionException("The requested operation does not exist.")
