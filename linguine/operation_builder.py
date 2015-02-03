a47bfd12f88c2343a0e1d1c98153f1f87feff160
Mike Wideman <wideman.mb@gmail.com>
2015-02-03 12:52:32 -0500

    Adding word cloud op
    Basically just a simpler tfidf

--- linguine/operation_builder.py
+++ linguine/operation_builder.py
@@ -2,9 +2,13 @@ import sys
 from linguine.transaction_exception import TransactionException
 from linguine.ops.tfidf import Tfidf
 from linguine.ops.no_op import NoOp
-
+from linguine.ops.word_cloud_op import WordCloudOp
 def get_operation_handler(operation):
     if operation == 'tfidf':
         return Tfidf()
+    elif operation == 'wordcloudop':
+    	return WordCloudOp()
+    elif operation == 'noop':
+    	return NoOp()
     else:
         raise TransactionException("The requested operation does not exist.")
---
+++ linguine/ops/word_cloud_op.py
@@ -0,0 +1,22 @@
+from nltk import word_tokenize
+class WordCloudOp:
+
+    def run(self, data):
+        terms = { }
+        results = [ ]
+        try:
+            for corpus in data:
+                tokens = word_tokenize(corpus.contents)
+                for token in tokens:
+                    if token in terms:
+                        terms[token]+=1
+                    else:
+                        terms[token]=1
+            for term in terms:
+                results.append({ "term" : term, "frequency" : terms[term]})
+            return results
+        except LookupError:
+            raise TransactionException('NLTK \'Punkt\' Model not installed.', 500)
+        except TypeError:
+            raise TransactionException('Corpus contents does not exist.')
+
