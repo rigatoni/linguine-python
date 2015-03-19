0a97195dc8bb5f6caf68d84b96631c26e0c8c348
danrlavoie <dan.r.lavoie+github@gmail.com>
2015-03-19 12:45:30 -0400

    Add comment clarifying how it operates.

--- linguine/untokenize.py
+++ linguine/untokenize.py
@@ -1,3 +1,6 @@
 import string
 def untokenize(tokens):
+	#Joins all tokens in the list into a single string.
+	#If a token doesn't start with an apostrophe and isn't a punctuation mark, add a space in front of it before joining.
+	#Then strip the total string so that leading spaces and trailing spaces are removed.
 	return "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in tokens]).strip()
