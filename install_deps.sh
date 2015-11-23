b31f52dd849ec53b567096cf4417bbc2609e8f6b
jmp3833 <jmp3833@rit.edu>
2015-11-23 14:35:33 -0500

    Add corenlp install process to dpendencies for project
    In order to install the corenlp wrapper of choice,
    a manual install from github is required. This has been placed
    in a bash script now to make it easier for deployment.

---
+++ install_deps.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/brendano/stanford_corenlp_pywrapper
+cd stanford_corenlp_pywrapper
+pip install .
+
+cd ..
+rm -rf stanford_corenlp_pywrapper
+pip install -r requirements.txt
+
