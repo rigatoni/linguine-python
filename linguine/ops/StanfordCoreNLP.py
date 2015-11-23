8ba03f6be64ee12634183e0b5c5f3aa3b6014b94
jmp3833 <jmp3833@rit.edu>
2015-11-23 15:28:02 -0500

    Add coreNLP models jar relative path  as well

--- linguine/ops/StanfordCoreNLP.py
+++ linguine/ops/StanfordCoreNLP.py
@@ -11,11 +11,10 @@ class StanfordCoreNLP:
     def __init__(self):
         # I don't see anywhere to put properties like this path...
         # For now it's hardcoded and would need to be changed when deployed...
-        print "Some stuff"
-        print os.path.abspath(__file__)
+        
         coreNLPPath = os.path.join(os.path.dirname(__file__), '../../lib/stanfordCoreNLP.jar')
-        print coreNLPPath
-        self.proc = CoreNLP('pos', corenlp_jars=[coreNLPPath])
+        coreNLPModelsPath = os.path.join(os.path.dirname(__file__), '../../lib/stanfordCoreNLPModels.jar')
+        self.proc = CoreNLP('pos', corenlp_jars=[coreNLPPath, coreNLPModelsPath])
 
     def run(self, data):
         results = []
