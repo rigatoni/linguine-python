a09f08fd16a6661572f5ff5d7830eb389f74c5c7
jmp3833 <jmp3833@rit.edu>
2015-11-30 10:37:01 -0500

    Cleanup to ensure support on python3.5

--- install_deps.sh
+++ install_deps.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/brendano/stanford_corenlp_pywrapper
 cd stanford_corenlp_pywrapper
-pip install .
+pip-python3 install .
 
 cd ..
 rm -rf stanford_corenlp_pywrapper
-pip install -r requirements.txt
+pip-python3 install -r requirements.txt
 
