ec518acae8db5c3c5ae2c738163f17dc395c0086
jmp3833 <jmp3833@rit.edu>
2016-01-16 12:38:09 +0700

    Cleanup home directory to work with nose

--- install_deps.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/brendano/stanford_corenlp_pywrapper
-cd stanford_corenlp_pywrapper
-pip-python3 install .
-
-cd ..
-rm -rf stanford_corenlp_pywrapper
-pip-python3 install -r requirements.txt
-
