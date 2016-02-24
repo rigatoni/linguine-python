70601c929593ca8d122d259caa711493dc4b19b3
jmp3833 <jmp3833@rit.edu>
2016-02-23 22:39:44 -0500

    Remove redundant print and add service logging

--- linguine/webserver.py
+++ linguine/webserver.py
@@ -18,17 +18,24 @@ except ImportError:
     sys.stderr.write("Tornado not found.")
 
 class MainHandler(tornado.web.RequestHandler):
+
     def post(self):
         self.set_header('Content-Type', 'application/json')
         try:
+            print(self.request.body)
             transaction = Transaction()
             transaction.parse_json(self.request.body)
             self.write(transaction.run())
         except TransactionException as err:
+
+            print("===========error==================")
+            print(json.JSONEncoder().encode({'error': err.error})) 
+            print("===========end_error==================")
             self.set_status(err.code)
             self.write(json.JSONEncoder().encode({'error': err.error}))
 
 if __name__ == "__main__":
+
     try:
         application = tornado.web.Application([(r"/", MainHandler)])
         application.listen(5555)
